00957c1984e8d77c7277e9702a7f8e760fc9ead6
krishna sharma <74679038+1krishnasharma@users.noreply.github.com>
2023-01-17 12:56:31 +0530

    python file

---
+++ whatauto.py
@@ -0,0 +1,6 @@
+import pywhatkit
+pywhatkit.sendwhatmsg('+91--------', 'Happy birthday ---', 7, 0,15, True, 2)
+# msg will be send at 7:00
+
+pywhatkit.sendwhatmsg('+91--------', 'Type your massage whatever you want', 7, 15,15,True,2)
+# msg will be send at 7:15
